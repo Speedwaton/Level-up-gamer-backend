275c33292f75a47b5d3978a0e551739840f339cc
Agustin Moya <agumoya889@gmail.com>
2025-12-02 21:52:28 -0300

    feat: Configurar seguridad, CORS y carga inicial de datos

---
+++ src/test/java/com/levelup/gamer/LevelupgamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelup.gamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelupgamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
